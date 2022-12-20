7ec98f779794687b83ae85dc327e99e354f90834
leo-vitor <leo.vitor@alu.ufc.br>
2022-12-20 15:58:19 -0300

    2 files modified

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -9,30 +9,28 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class Tab3Page implements OnInit {
   form: FormGroup;
 
-  constructor(
-    private formBuilder: FormBuilder,
-  ) {}
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit() {
     this.form = this.formBuilder.group({
       nome: ['', [Validators.required]],
       endereco: ['', [Validators.required]],
     });
-
-    
   }
 
-  whats() {
-    this.openURL(
-      ' https://api.whatsapp.com/send?phone=555194904309&text=Ol%C3%A1%20vim%20atraves%20do%20seu%20App%20Gostaria%20de%20receber%20doacoes'
-    );
-  }
+  whats() {}
 
   openURL(url: string) {
     window.open(url, '_blank').focus();
   }
 
-  submit(){
-    console.log('enviado')
+  submit() {
+    let nome = this.form.get('nome').value;
+    let endereco = this.form.get('endereco').value;
+    console.log(nome);
+
+    this.openURL(
+      `https://api.whatsapp.com/send?phone=555194904309&text=Ola%20me%20chamo%20${nome}%20moro%20em%20${endereco}%20vim%20atraves%20do%20seu%20App%20Gostaria%20de%20receber%20doacoes`
+    );
   }
 }
