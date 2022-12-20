221c03db9e954ff2d994bd98d4b4575a8cd9d81d
leo-vitor <leo.vitor@alu.ufc.br>
2022-12-20 14:57:33 -0300

    2 files modified

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -3,10 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
-  styleUrls: ['tab3.page.scss']
+  styleUrls: ['tab3.page.scss'],
 })
 export class Tab3Page {
-
   constructor() {}
 
+  whats() {
+    this.openURL(
+      ' https://api.whatsapp.com/send?phone=555194904309&text=Ol%C3%A1%20vim%20atraves%20do%20seu%20App%20Gostaria%20de%20receber%20doacoes'
+    );
+  }
+
+  openURL(url: string) {
+    window.open(url, '_blank').focus();
+  }
 }
