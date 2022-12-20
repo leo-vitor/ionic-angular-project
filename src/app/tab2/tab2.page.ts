9b32327b3f4dfa9845f629770d1051a996654b52
leo-vitor <leo.vitor@alu.ufc.br>
2022-12-20 14:46:14 -0300

    2 files modified

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -3,10 +3,30 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
 })
 export class Tab2Page {
-
   constructor() {}
 
+  donateFood() {
+    this.openURL(
+      'https://www.google.com/maps/search/?api=1&query=Ponto+Doacao+Alimento'
+    );
+  }
+
+  donateClothes() {
+    this.openURL(
+      'https://www.google.com/maps/search/?api=1&query=Ponto+Doacao+Roupas'
+    );
+  }
+
+  donateBlood() {
+    this.openURL(
+      'https://www.google.com/maps/search/?api=1&query=Ponto+Doacao+Sangue'
+    );
+  }
+
+  openURL(url: string) {
+    window.open(url, '_blank').focus();
+  }
 }
